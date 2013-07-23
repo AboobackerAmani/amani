efaf2deaed12f101db9c5ccef7b129cff885e1c8
Ben St John <Ben@affinitybridge.com>
2013-07-22 17:41:01 -0700

    See #117. Removed sym links from original profile and created directories containing modules themes etc.Also updated amani news & events blog feature to properly display blog images after a clean install.

---
+++ core/profiles/amani/themes/peace/templates/views/views-view-fields--front-page-slideshow.tpl.php
@@ -0,0 +1,14 @@
+<?php foreach ($fields as $id => $field): ?>
+  <?php $item[$id] = $field->content; ?>
+<?php endforeach; ?>
+
+<?php //krumo($item); ?>
+<div class="slideshow-item">
+  <div class="left-col">
+    <?php echo $item['field_image']; ?>
+  </div>
+  <div class="right-col">
+    <h2><?php echo $item['title']; ?></h2>
+    <?php echo $item['field_description']; ?>
+  </div>
+</div>
