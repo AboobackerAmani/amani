380e4456648e6b79ce4b460c2c4578f1449b4e4a
Alberto Trujillo Gonzalez <altrugon@gmail.com>
2015-04-20 10:34:27 -0700

    Rename theme pg2015 to amani_zen.

--- amani/themes/amani_zen/template.php
+++ amani/themes/amani_zen/template.php
@@ -17,12 +17,12 @@
  *   The name of the template being rendered ("maintenance_page" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function pg2015_preprocess_maintenance_page(&$variables, $hook) {
+function amani_zen_preprocess_maintenance_page(&$variables, $hook) {
   // When a variable is manipulated or added in preprocess_html or
   // preprocess_page, that same work is probably needed for the maintenance page
   // as well, so we can just re-use those functions to do that work here.
-  pg2015_preprocess_html($variables, $hook);
-  pg2015_preprocess_page($variables, $hook);
+  amani_zen_preprocess_html($variables, $hook);
+  amani_zen_preprocess_page($variables, $hook);
 }
 // */
 
@@ -34,14 +34,16 @@ function pg2015_preprocess_maintenance_page(&$variables, $hook) {
  * @param $hook
  *   The name of the template being rendered ("html" in this case.)
  */
-function pg2015_preprocess_html(&$variables, $hook) {
+function amani_zen_preprocess_html(&$variables, $hook) {
   // The body tag's classes are controlled by the $classes_array variable. To
   // remove a class from $classes_array, use array_diff().
   //$variables['classes_array'] = array_diff($variables['classes_array'], array('class-to-remove'));
   global $base_url;
 
   // Adding favicons and app icons
-  $theme_path = $base_url . '/' . drupal_get_path('theme', 'pg2015') . '/';
+  // $theme_path = $base_url . '/' . drupal_get_path('theme', 'amani_zen') . '/';
+  $theme_path = $base_url . '/' . drupal_get_path('theme',$GLOBALS['theme']) . '/';
+
   $icons = array(
     array('rel' => 'apple-touch-icon', 'href' => $theme_path . 'apple-icon-57x57.png', 'sizes' => '57x57'),
     array('rel' => 'apple-touch-icon', 'href' => $theme_path . 'apple-icon-60x60.png', 'sizes' => '60x60'),
@@ -57,7 +59,7 @@ function pg2015_preprocess_html(&$variables, $hook) {
     array('rel' => 'icon', 'href' => $theme_path . 'favicon-32x32.png', 'sizes' => '32x32', 'type' => 'image/png'),
     array('rel' => 'icon', 'href' => $theme_path . 'favicon-96x96.png', 'sizes' => '96x96', 'type' => 'image/png'),
   );
-
+var_dump($icons[0]);
   foreach ($icons as $icon) {
     drupal_add_html_head_link($icon);
   }
@@ -80,7 +82,7 @@ function pg2015_preprocess_html(&$variables, $hook) {
  *   The name of the template being rendered ("page" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function pg2015_preprocess_page(&$variables, $hook) {
+function amani_zen_preprocess_page(&$variables, $hook) {
   $variables['sample_variable'] = t('Lorem ipsum.');
 }
 // */
@@ -94,11 +96,11 @@ function pg2015_preprocess_page(&$variables, $hook) {
  *   The name of the template being rendered ("node" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function pg2015_preprocess_node(&$variables, $hook) {
+function amani_zen_preprocess_node(&$variables, $hook) {
   $variables['sample_variable'] = t('Lorem ipsum.');
 
   // Optionally, run node-type-specific preprocess functions, like
-  // pg2015_preprocess_node_page() or pg2015_preprocess_node_story().
+  // amani_zen_preprocess_node_page() or amani_zen_preprocess_node_story().
   $function = __FUNCTION__ . '_' . $variables['node']->type;
   if (function_exists($function)) {
     $function($variables, $hook);
@@ -115,7 +117,7 @@ function pg2015_preprocess_node(&$variables, $hook) {
  *   The name of the template being rendered ("comment" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function pg2015_preprocess_comment(&$variables, $hook) {
+function amani_zen_preprocess_comment(&$variables, $hook) {
   $variables['sample_variable'] = t('Lorem ipsum.');
 }
 // */
@@ -129,7 +131,7 @@ function pg2015_preprocess_comment(&$variables, $hook) {
  *   The name of the template being rendered ("region" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function pg2015_preprocess_region(&$variables, $hook) {
+function amani_zen_preprocess_region(&$variables, $hook) {
   // Don't use Zen's region--sidebar.tpl.php template for sidebars.
   //if (strpos($variables['region'], 'sidebar_') === 0) {
   //  $variables['theme_hook_suggestions'] = array_diff($variables['theme_hook_suggestions'], array('region__sidebar'));
@@ -146,7 +148,7 @@ function pg2015_preprocess_region(&$variables, $hook) {
  *   The name of the template being rendered ("block" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function pg2015_preprocess_block(&$variables, $hook) {
+function amani_zen_preprocess_block(&$variables, $hook) {
   // Add a count to all the blocks in the region.
   // $variables['classes_array'][] = 'count-' . $variables['block_id'];
 
--- amani/themes/amani_zen/theme-settings.php
+++ amani/themes/amani_zen/theme-settings.php
@@ -7,7 +7,7 @@
  * @param $form_state
  *   A keyed array containing the current state of the form.
  */
-function pg2015_form_system_theme_settings_alter(&$form, &$form_state, $form_id = NULL)  {
+function amani_zen_form_system_theme_settings_alter(&$form, &$form_state, $form_id = NULL)  {
   // Work-around for a core bug affecting admin themes. See issue #943212.
   if (isset($form_id)) {
     return;
@@ -16,10 +16,10 @@ function pg2015_form_system_theme_settings_alter(&$form, &$form_state, $form_id 
   // Create the form using Forms API: http://api.drupal.org/api/7
 
   /* -- Delete this line if you want to use this setting
-  $form['pg2015_example'] = array(
+  $form['amani_zen_example'] = array(
     '#type'          => 'checkbox',
-    '#title'         => t('pg2015 sample setting'),
-    '#default_value' => theme_get_setting('pg2015_example'),
+    '#title'         => t('amani_zen sample setting'),
+    '#default_value' => theme_get_setting('amani_zen_example'),
     '#description'   => t("This option doesn't do anything; it's just an example."),
   );
   // */
