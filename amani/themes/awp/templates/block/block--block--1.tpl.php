fad8815caa7a5dcec268fe16a1cb67bcd6ab2076
iMac <admin@beyondesigns.net>
2015-04-21 14:16:24 +0300

    adding tpl block for header blocks

---
+++ amani/themes/awp/templates/block/block--block--1.tpl.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kshihabi
+ * Date: 2/18/15
+ * Time: 11:16 PM
+ */
+
+//this block is not displayed anywhere. It is embedded in block--block--3.tpl.php
+//this block is for the top right menu in the website
+
+global $language;
+global $base_url;
+if($language->language=='en') {
+    $link=$base_url.'/ar';
+    $text=t("Arabic");
+}else {
+    $link=$base_url.'/en';
+    $text=t("English");
+}
+
+$block = module_invoke('search', 'block_view', 'search');
+$search_block='<div class="sub-head">' . render($block). '</div>';
+
+$block = module_invoke('menu', 'block_view', 'menu-social-media');
+$social_block='<div class="sub-head">' . render($block). '</div>';
+
+?>
+<div class="header_links">
+    <div class="link search_link"><a href="#">search</a><?php print $search_block;?></div>
+    <div class="social-block"><?php print $social_block;?></div>
+
+</div>
