1656455bb122681ffd260f722eb5042068eae0bb
iMac <admin@beyondesigns.net>
2015-07-11 16:00:55 +0300

    blog

---
+++ amani/themes/awp/templates/views/views-view-field--news-and-events-blog--page--nothing.tpl.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @file
+ * This template is used to print a single field in a view.
+ *
+ * It is not actually used in default Views, as this is registered as a theme
+ * function which has better performance. For single overrides, the template is
+ * perfectly okay.
+ *
+ * Variables available:
+ * - $view: The view object
+ * - $field: The field handler object that can process the input
+ * - $row: The raw SQL result that can be used
+ * - $output: The processed output that will normally be used.
+ *
+ * When fetching output from the $row, this construct should be used:
+ * $data = $row->{$field->field_alias}
+ *
+ * The above will guarantee that you'll always get the correct data,
+ * regardless of any changes in the aliasing that might happen if
+ * the view is modified.
+ */
+?>
+
+
+<?php $nid=$row->_field_data['nid']['entity']->nid;
+$mynode=node_load($nid);
+
+
+global $language;
+$url = url('node/'.$nid, array('language' => $language));
+
+?>
+
+
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=251571348195922";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
+<?php
+$attached_links = '<span class="fb-share-button"  data-href="'.$url.'" data-layout="button"></span>  <a href="https://twitter.com/share" class="twitter-share-button" data-url="'.$url.'" data-count="none">Tweet</a>';
+?>
+
+
+
+
+<?php print $output.' '.$attached_links; ?>
