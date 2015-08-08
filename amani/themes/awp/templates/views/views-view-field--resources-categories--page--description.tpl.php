d9e76979c93782fa23c0f9104589a8ddec08288e
iMac <admin@beyondesigns.net>
2015-08-08 14:09:26 +0300

    resource term view

--- amani/themes/awp/templates/views/views-view-field--resources-categories--page--description.tpl.php
+++ amani/themes/awp/templates/views/views-view-field--resources-categories--page--description.tpl.php
@@ -4,6 +4,5 @@ $term = taxonomy_term_load($tid);
 $translated_term = i18n_taxonomy_localize_terms($term);
 //print $translated_term->name;
 print $translated_term->description;
-print_r $translated_term->description;
 
 ?>
