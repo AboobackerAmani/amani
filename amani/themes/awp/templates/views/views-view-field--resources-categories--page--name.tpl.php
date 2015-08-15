5b2925de91f099c857fe920e4a93edc447ba0af6
iMac <admin@beyondesigns.net>
2015-08-15 21:16:16 +0300

    css resources mobile

--- amani/themes/awp/templates/views/views-view-field--resources-categories--page--name.tpl.php
+++ amani/themes/awp/templates/views/views-view-field--resources-categories--page--name.tpl.php
@@ -2,7 +2,8 @@
 $tid =  $row->tid;
 $term = taxonomy_term_load($tid);
 $translated_term = i18n_taxonomy_localize_terms($term);
-print strip_tags($translated_term->name);
+//print strip_tags($translated_term->name);
+print '<a href="./'.$tid.'">'.strip_tags($translated_term->name).'</a>';
 //print $translated_term->description;
 
 ?>
