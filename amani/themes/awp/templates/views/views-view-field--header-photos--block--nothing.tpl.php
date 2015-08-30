bc1bc1facbbd9e771dd582491675a9f5bcfcc9ab
iMac <admin@beyondesigns.net>
2015-08-30 19:51:15 +0300

    header photos block

--- amani/themes/awp/templates/views/views-view-field--header-photos--block--nothing.tpl.php
+++ amani/themes/awp/templates/views/views-view-field--header-photos--block--nothing.tpl.php
@@ -7,6 +7,15 @@ $path_alias=drupal_get_path_alias();
 //exit();
 
 $query = new EntityFieldQuery();
+$entities = $query->entityCondition('entity_type', 'node')
+    ->entityCondition('bundle', 'header_photos')
+    ->propertyCondition('status', 1)
+    ->fieldCondition('field_view_in_path_', 'value',$path_alias)
+    ->execute();
+$nodes = entity_load('node', array_keys($entities['node']));
+
+
+$nodes = entity_load('node', array_keys($entities['node']));
 
 return node_view_multiple($nodes, 'full');
 
