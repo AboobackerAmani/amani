ccdf124a68ca0ba2587c371aea82d1d6c4240efe
iMac <admin@beyondesigns.net>
2015-08-30 19:38:00 +0300

    header photos block

--- amani/themes/awp/templates/views/views-view-field--header-photos--block--nothing.tpl.php
+++ amani/themes/awp/templates/views/views-view-field--header-photos--block--nothing.tpl.php
@@ -1,6 +1,10 @@
 <?php $nid=$row->_field_data['nid']['entity']->nid;
-print $nid;
+//print $nid;
+//$mynode=node_load($nid);
+
+$path=current_path();
+print $path;
 exit();
-$mynode=node_load($nid);
+
 ?>
 
