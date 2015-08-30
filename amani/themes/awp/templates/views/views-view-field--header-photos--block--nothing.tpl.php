860aeee1a923a1461e55b29927206ceeb48cb923
iMac <admin@beyondesigns.net>
2015-08-30 21:35:00 +0300

    header photos block

--- amani/themes/awp/templates/views/views-view-field--header-photos--block--nothing.tpl.php
+++ amani/themes/awp/templates/views/views-view-field--header-photos--block--nothing.tpl.php
@@ -23,11 +23,10 @@ foreach($nodes as $node) {
 
     $style = 'header_photo';
     $styled_image= image_style_url($style, $imageone);
-    if (isset($node->field_image_link) ) {
         if (!empty($node->field_image_link)){
             $image_link = $node->field_image_link[LANGUAGE_NONE][0][url];
             $link_target = $node->field_image_link[LANGUAGE_NONE][0][attributes][target];
-            print_r($node->field_image_link);
+            //print_r($node->field_image_link);
             //exit();
             if ($link_target=="0"){
                 $html_image='<a href="'.$image_link.'"><img src="'.$styled_image.'"></a>';
@@ -36,7 +35,6 @@ foreach($nodes as $node) {
                 $html_image='<a href="'.$image_link.'" target="'.$link_target.'"><img src="'.$styled_image.'"></a>';
             }
         }//if (!empty($node->field_image_link)){
-    }//if (isset($node->field_image_link) ) {
     else {
         $html_image = '<img src="' . $styled_image . '">';
     }
