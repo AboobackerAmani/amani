eccd02bc7ace8cb586ab7fb9db6d896bf243560c
iMac <admin@beyondesigns.net>
2015-09-05 16:14:58 +0300

    critique

--- amani/themes/awp/templates/node/node--critique.tpl.php
+++ amani/themes/awp/templates/node/node--critique.tpl.php
@@ -133,7 +133,7 @@ else{ ?>
                             </a></div>  </div>
                     <div class="views-field views-field-title">        <span class="field-content"><a href="<?php if(!empty($node->field_trucks_link)){
                                 print ($node->field_trucks_link[und][0][url]);
-                            }?>"><?php print t('Trucks Delivered vs. Needed');?></a></span>
+                            }?>"><?php print t('Percentage of materials needed that have entered');?></a></span>
                     </div>
                     <div class="views-field views-field-field-text">
                         <div class="field-content">
@@ -221,7 +221,7 @@ else{ ?>
                             </a></div>  </div>
                     <div class="views-field views-field-title">        <span class="field-content"><a href="    <?php if(!empty($node->field_home_link)):
                                 print ($node->field_home_link[und][0][url]);
-                            endif;?>"><?php print t('Homes repaired and rebuilt');?> </a></span>  </div>
+                            endif;?>"><?php print t('Homes repaired and rebuilt with aid');?> </a></span>  </div>
                     <div class="views-field views-field-field-text">
                         <div class="field-content">  <?php if(!empty($node->field_home_rebuilt)):
                                 print $node->field_home_rebuilt[und][0][value];
