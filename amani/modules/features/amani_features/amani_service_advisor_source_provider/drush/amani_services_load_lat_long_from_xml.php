99900997b908dd300e75e6a8b02ade9a247c4de6
Floyd Mann <floyd@affinitybridge.com>
2016-02-24 10:54:35 -0800

    More location importing.

---
+++ amani/modules/features/amani_features/amani_service_advisor_source_provider/drush/amani_services_load_lat_long_from_xml.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * Import vocabularies for Service Advisor content types.
+ *
+ * Usage: drush scr profiles/amani/modules/features/amani_features/amani_service_advisor_source_provider/drush/amani_services_load_terms.php [optional_vocabulary_name]
+ *
+ */
+
+module_load_include('inc', 'amani_service_advisor_source_provider', 'includes/amani_service_advisor_source_provider.api');
+
+amani_service_advisor_source_provider_load_lat_long_from_xml();
