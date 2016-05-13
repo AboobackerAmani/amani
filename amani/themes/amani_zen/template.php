71d8452f535d1a19882d2db4aad417a94006c086
Ben St John <ben@affinitybridge.com>
2016-05-13 14:42:43 -0700

    Fixing date on mini calendar view.

--- amani/themes/amani_zen/template.php
+++ amani/themes/amani_zen/template.php
@@ -214,15 +214,26 @@ function amani_zen_form_alter(&$form, &$form_state, $form_id) {
 }
 
 /**
- * Customize the calendar pager
+ * Use mini pager for all date views.
  */
 function amani_zen_preprocess_date_views_pager(&$vars) {
-  $datetime = DateTime::createFromFormat('F Y', $vars['nav_title']);
-  $month = $datetime ? $datetime->format('F') : NULL;
-  $year = $datetime ? $datetime->format('Y') : NULL;
-  $vars['nav_title'] = "<span class='month'>$month</span> - $year";
-
   $vars['mini'] = TRUE;
+}
+
+/**
+ * We use the mini calendar which doesn't by default include the year, do so here.
+ */
+function amani_zen_date_nav_title($params) {
+  // Block 3 is our calendar block.
+  if ($params['view']->current_display == 'block_3' && $params['granularity'] == 'month') {
+    if ($date_info = $params['view']->date_info) {
+      $month_num = $date_info->month;
+      $date_obj = DateTime::createFromFormat('!m', $month_num);
+      $month = $date_obj->format('F');
+      $year = $date_info->year;
+      return '<span class="month">' . $month . '</span> -' . $year;
+    }
+  }
 }
 
 /**
