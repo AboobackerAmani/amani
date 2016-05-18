d877ca501765384fbf24f99864903d4452e7abc2
Gurinder Hans <gurinder@appnovation.com>
2016-05-18 14:48:44 -0700

    fix #1205 - added links to each of the taxonomy terms

--- amani/themes/amani_zen/templates/node--resource.tpl.php
+++ amani/themes/amani_zen/templates/node--resource.tpl.php
@@ -22,16 +22,14 @@
         <?php print render($content['field_date']); ?>
       </p>
       <p>
-        <?php
-          if (!empty($node->field_resource_type))
-            print $node->field_resource_type['und'][0]['taxonomy_term']->name;
-        ?>
+        <?php if (!empty($node->field_resource_type)): ?>
+          <a href="<?php print taxonomy_term_uri($node->field_resource_type['und'][0]['taxonomy_term'])['path']; ?>"><?php print $node->field_resource_type['und'][0]['taxonomy_term']->name; ?></a>
+        <?php endif; ?>
       </p>
       <p>
-        <?php
-          if (!empty($node->field_theme_s_))
-            print $node->field_theme_s_['und'][0]['taxonomy_term']->name;
-        ?>
+        <?php if (!empty($node->field_theme_s_)): ?>
+          <a href="<?php print taxonomy_term_uri($node->field_theme_s_['und'][0]['taxonomy_term'])['path']; ?>"><?php print $node->field_theme_s_['und'][0]['taxonomy_term']->name; ?></a>
+        <?php endif; ?>
       </p>
       <p>
         <?php print render($content['field_location_name']); ?>
